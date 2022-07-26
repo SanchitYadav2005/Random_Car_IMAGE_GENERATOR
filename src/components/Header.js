f16dbb3ee862e225202ee47ee9ab9f4362e573b0
SanchitYadav2005 <sanchit2005yudavnashi@gmail.com>
2022-07-26 16:54:26 +0530

    done with designing the header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Header(){
+    return(
+        <nav className="nav-bar">
+            <img src="https://media.istockphoto.com/photos/car-racing-picture-id180716173?b=1&k=20&m=180716173&s=170667a&w=0&h=VnCC6dJAsTLyk7IPmvLcukN-9fhqRxYkRXpFKlG9t8A=" alt="car logo"/>
+            <h1>Car Images</h1>
+        </nav>
+    )
+}
+
+export default Header;
